9cf9175fdb22567e69e9cd1888eed8e9d4353f6a
Abraham Esparza <abrahamesparza.dev@gmail.com>
2025-01-08 17:33:54 -0800

    Add home directory

---
+++ src/app/home/page.js
@@ -0,0 +1,11 @@
+'use client';
+import styles from './home.module.css';
+
+export default function Home() {
+
+    return (
+        <div className={styles.container}>
+            <h1>Home</h1>
+        </div>
+    )
+}
