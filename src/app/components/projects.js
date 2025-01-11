a1e13110e05d7a68f44651cd7ee5b723a34ac14c
Abraham Esparza <abrahamesparza.dev@gmail.com>
2025-01-10 17:03:57 -0800

    Add components folder and projects component

---
+++ src/app/components/projects.js
@@ -0,0 +1,12 @@
+'use client';
+import styles from './components.module.css';
+
+export default function Projects() {
+
+    return (
+        <div className={styles.projectContainer}>
+            <h1>Description of project here</h1>
+            <h1>Video/gif of project here</h1>
+        </div>
+    )
+}
