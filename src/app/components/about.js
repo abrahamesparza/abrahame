529b100e9c328988d0f0b8d3b202239ab95550b9
Abraham Esparza <abrahamesparza.dev@gmail.com>
2025-02-12 22:23:28 -0800

    Add brief about component, update css

---
+++ src/app/components/about.js
@@ -0,0 +1,13 @@
+'use client';
+import styles from './components.module.css';
+
+export default function About () {
+    return (
+        <div className={styles.aboutContainer}>
+            <p>
+                My journey into engineering may not be traditional, but that doesn't define me, or limit my abilities, curiosity and eagerness for learning and continuous self improvement.
+                When I'm not working, spending time with my family, or working on my overall health, I'm either solving LeetCode problems, or working on a full-stack project.
+            </p>
+        </div>
+    )
+}
