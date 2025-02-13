cbbbc7a8cd435c8958762f12f2803bdc20a82dbe
Abraham Esparza <abrahamesparza.dev@gmail.com>
2025-02-12 22:24:12 -0800

    Update about component

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -7,6 +7,7 @@ export default function About () {
             <p>
                 My journey into engineering may not be traditional, but that doesn't define me, or limit my abilities, curiosity and eagerness for learning and continuous self improvement.
                 When I'm not working, spending time with my family, or working on my overall health, I'm either solving LeetCode problems, or working on a full-stack project.
+                
             </p>
         </div>
     )
