e180d37cf0a0657beca0830845770884bfbeff01
Abraham Esparza <abrahamesparza.dev@gmail.com>
2025-02-28 11:28:54 -0800

    Add list options and rerouting

--- src/app/components/navigation.js
+++ src/app/components/navigation.js
@@ -1,13 +1,24 @@
 'use client';
 import styles from './components.module.css';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Navigation () {
-    const listOptions = ['PHILEOTHEO', 'STORE'];
+    const router = useRouter();
+    const listOptions = ['FAITH', 'FAMILY', 'PROFESSIONAL', 'BUSINESS'];
+
+    const handlePageRender = (e) => {
+        const page = e.target.innerText;
+        if (page === 'PROFESSIONAL') {
+            router.push('/professional');
+        }
+    }
+
     return (
-        <div className={styles.navigationContainer}>
-            <ul>
+        <div>
+            <ul className={styles.navList}>
                 {listOptions.map((item, index) => (
-                    <li key={index}>{item}</li>
+                    <li className={styles.navItems} key={index} onClick={handlePageRender}>{item}</li>
                 ))}
             </ul>
         </div>
