2a26cc1e3737d90aa56c3a3e74f3489b0cb2cec6
Abraham Esparza <abrahamesparza.dev@gmail.com>
2025-03-03 23:24:04 -0800

    Add util function to send an email

---
+++ src/app/utils/send-email.js
@@ -0,0 +1,19 @@
+export function sendEmail(data) {
+    const apiEndpoint = '/api/email';
+  
+    fetch(apiEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        alert(response.message);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+  
